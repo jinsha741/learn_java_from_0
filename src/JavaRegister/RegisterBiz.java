b8223980c7a5a63ec820fcf451ca8295e74cef9d
HuangYL <HuangYL@192.168.0.162>
2018-11-16 18:41:41 +0900

    javaRegister Update in 2018.11.16

--- src/JavaRegister/RegisterBiz.java
+++ src/JavaRegister/RegisterBiz.java
@@ -30,7 +30,7 @@ public class RegisterBiz {
 		
 		if(userName.length() <= 0 && userName.length() > 25) {
 			System.out.println("输入的用户名必须在1-25字符之间，请重新输入");
-			registerUserName();	
+			registerUserName();
 		}else if(isLetterOrDigitUserName(ch) == false) {
 			System.out.println("输入的用户名含有特殊字符或空格，请重新输入");
 			registerUserName();
@@ -44,7 +44,7 @@ public class RegisterBiz {
 	 * 6-18位，只能包含数字，字母，下划线_，
      *        纯数字或纯字母    ：强度弱：(★★☆☆☆☆)
      *        数字和字母       ：强度中：(★★★★☆☆)
-     *        数字和字母和下划线 ：强度高：(★★★★★★)
+     *        数字或字母和下划线 ：强度高：(★★★★★★)
 	 * @param password
 	 */
 	public void registerPassword() {
@@ -63,19 +63,103 @@ public class RegisterBiz {
 			System.out.println("密码合法，密码为：" + register.getPassword());
 		}
 		
-		//密码强度表示
-		int strength = passwordStrength(ch);
-		if(strength == 1) {
+		/**
+		 * 纯数字或纯字母    ：强度弱：(★★☆☆☆☆)
+	     * 数字和字母       ：强度中：(★★★★☆☆)
+	     * 数字和字母和下划线 ：强度高：(★★★★★★)
+		 */
+		if(isDigit(password) || isLetter(password)) {
 			System.out.println("密码强度低：★★☆☆☆☆");
-		}else if(strength == 2) {
+		}else if(isDigitAndLetter(password)) {
 			System.out.println("密码强度中：★★★★☆☆");
-		}else {
+		}else if(isContainsSymbol(password)){
 			System.out.println("密码强度高：★★★★★★");
 		}
 	}
+
 	
+	/**
+	 * email：必须包含@和.符号。
+     * 判断@ 符号有且仅有一个
+     * @ 和 .符号不能在最前和最后
+     * 
+     * ****方法不完全，还是有漏洞*********
+     * 1， 没有判断@和.不能连在一起
+     * 2，没有判断@后面必须含有.
+	 */
 	public void registerEmail() {
+		System.out.print("请输入邮箱：");
+		String email = new Scanner(System.in).nextLine();
 		
+		if(!isContainsAtOrDotSymbol(email) == false) {
+			System.out.println("必须包含@和.符号,请重新输入");
+			registerEmail();
+		}else if(isAtsymbolOnlyOne(email) == false) {
+			System.out.println("@符号有且仅有一个,请重新输入");
+			registerEmail();
+		}else if(isNotfrontOrEnd(email) == false) {
+			System.out.println("@ 和 .符号不能在最前和最后,请重新输入");
+			registerEmail();
+		}else {
+			register.setEmail(email);
+			System.out.println("email合法，email为：" + register.getEmail());
+		}
+	}
+	
+	/**
+	 * email：必须包含@和.符号
+	 * @param value
+	 * @return
+	 */
+	public static boolean isContainsAtOrDotSymbol(String value) {
+		String symbol1 = "@";
+		String symbol2 = ".";
+		for (int i = 0; i < value.length(); i++) {
+			if(!(symbol1.contains(Character.toString(value.charAt(i))))){
+				return false;
+			}
+		}
+		for (int i = 0; i < value.length(); i++) {
+			if(!(symbol2.contains(Character.toString(value.charAt(i))))){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * 判断@ 符号有且仅有一个
+	 * @param value
+	 * @return
+	 */
+	public static boolean isAtsymbolOnlyOne(String value) {
+		char ch[] = value.toCharArray();
+		int num = 0;
+		for (int i = 0; i < ch.length; i++) {
+			if(ch[i] == '@') {
+				num++;
+			}
+		}
+		if(num != 1) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * @ 和 .符号不能在最前和最后
+	 * @param value
+	 * @return
+	 */
+	public static boolean isNotfrontOrEnd(String value) {
+		int num1 = value.indexOf("@"); //前面方法已经可以判断@是否只有一个，所以不用lastIndexOf()
+		int num2 = value.indexOf(".");
+		int num3 = value.lastIndexOf(".");
+		if(num1 == 0 || num2 == 0 || num1 == (value.length() - 1) || num3 == (value.length() - 1)) {
+			return false;
+		}
+		return true;
 	}
 	
 	/**
@@ -131,30 +215,62 @@ public class RegisterBiz {
 		}	
 	}
 	
+	
+	
 	/**
-	 * 纯数字或纯字母    ：强度弱：(★★☆☆☆☆)
-     * 数字和字母       ：强度中：(★★★★☆☆)
-     * 数字和字母和下划线 ：强度高：(★★★★★★)
-	 * @param password
+	 * 判断是不是一个纯数字字符数组
+	 * @param ch
 	 * @return
 	 */
-	public int passwordStrength(char ch[]) {
-		int strength = 0;
-		
-		for (int i = 0; i < ch.length; i++) {
-			if(Character.isDigit(ch[i]) || Character.isLetter(ch[i])) {
-				strength = 1;
-			}
-			else if(Character.isLetter(ch[i]) && Character.isDigit(ch[i])) {
-				strength = 2;
-			}else {
-				strength = 3;
+	public static boolean isDigit(String value) {
+		for (int i = 0; i < value.length(); i++) {
+			if(!Character.isDigit(value.charAt(i))) {
+				return false;
 			}
 		}
-		return strength;
+		return true;
 		
 	}
-
 	
+	
+	/**
+	 * 判断是不是一个纯字母字符数组
+	 * @param ch
+	 * @return
+	 */
+	public static boolean isLetter(String value) {
+		for (int i = 0; i < value.length(); i++) {
+			if(!Character.isLetter(value.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+		
+	}
+	
+	/**
+	 * 判断字符串中是不是含有_
+	 * @param ch
+	 * @return
+	 */
+	public static boolean isContainsSymbol(String value) {
+		String symbol = "_";
+		for (int i = 0; i < value.length(); i++) {
+			if(symbol.contains(Character.toString(value.charAt(i)))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean isDigitAndLetter(String value) {
+		for (int i = 0; i < value.length(); i++) {
+			if(!Character.isLetterOrDigit(value.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	
 }
--- src/JavaRegister/RegisterText.java
+++ src/JavaRegister/RegisterText.java
@@ -4,8 +4,19 @@ public class RegisterText {
 
 	public static void main(String[] args) {
 		RegisterBiz registerBiz = new RegisterBiz();
-//		registerBiz.registerUserName();
+		Register register = new Register();
+		registerBiz.registerUserName();
 		registerBiz.registerPassword();
+		registerBiz.registerEmail();
+		
+		System.out.println("注册成功");
+		
+		/** 问题：以下调用get返回null */
+		/** 没有set? mian函数里怎么set？ */
+		
+//		System.out.println("账号：" + register.getUserName());
+//		System.out.println("密码：" + register.getPassword());
+//		System.out.println("邮箱：" + register.getEmail());
 
 	}
 
